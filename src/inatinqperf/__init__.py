8f51805009e11757cd44bd12c6a45238db4f4b77
Ketan Bhardwaj <ketanbj@cc.gatech.edu>
2025-09-17 10:14:14 -0400

    updated version - format check

--- src/inatinqperf/__init__.py
+++ src/inatinqperf/__init__.py
@@ -1,1 +1,9 @@
 """__init__.py for inatinqperf package."""
+
+try:
+    from importlib.metadata import version
+
+    __version__ = version("inatinqperf")
+except Exception:
+    __version__ = "unknown"
+# -----------------------

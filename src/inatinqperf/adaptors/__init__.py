22fdfbc0037363407e06c1284d1f022b80ef9a4d
varun646 <varun646@gmail.com>
2025-10-16 16:55:05 -0400

    address ruff errors

--- src/inatinqperf/adaptors/__init__.py
+++ src/inatinqperf/adaptors/__init__.py
@@ -1,9 +1,9 @@
 """Adaptor registry for vector databases."""
 
 from inatinqperf.adaptors.faiss_adaptor import Faiss
+from inatinqperf.adaptors.milvus_adaptor import Milvus
 from inatinqperf.adaptors.qdrant_adaptor import Qdrant
 from inatinqperf.adaptors.weaviate_adaptor import Weaviate
-from inatinqperf.adaptors.milvus_adaptor import Milvus
 
 VECTORDBS = {
     "faiss": Faiss,

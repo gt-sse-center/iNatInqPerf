ac551d968d9a962a223331c795b952f81e817fef
Varun Agrawal <varagrawal@gmail.com>
2025-11-03 22:24:20 -0500

    Add end to end benchmark tests

---
+++ tests/test_end_to_end.py
@@ -0,0 +1,20 @@
+"""Unit tests to verify end to end running of the benchmark."""
+
+import pytest
+
+from inatinqperf.benchmark import Benchmarker
+
+
+@pytest.mark.parametrize(
+    "config_filename",
+    [
+        "inquire_qdrant.yaml",
+        "inquire_milvus.yaml",
+        "inquire_weaviate.yaml",
+    ],
+)
+def test_full_run(fixtures_dir, config_filename):
+    config_file = fixtures_dir / config_filename
+
+    benchmarker = Benchmarker(config_file)
+    benchmarker.run()

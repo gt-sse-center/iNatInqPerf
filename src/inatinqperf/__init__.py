3e7ccf8e7c2422bfa3e53b5374d2d321b226cb5f
Varun Agrawal <varagrawal@gmail.com>
2025-11-04 23:35:20 -0500

    Upgrade huggingface datasets package to v4.4.0

--- src/inatinqperf/__init__.py
+++ src/inatinqperf/__init__.py
@@ -4,10 +4,17 @@ from importlib.metadata import version
 
 __version__ = version("inatinqperf")
 
+from sys import platform
+
 # On MacOS multiple linking of OpenMP happens and causes the program to segfault.
 # Setting the KMP_DUPLICATE_LIB_OK environment variable seems to be the best workaround.
 # Please see: https://github.com/dmlc/xgboost/issues/1715
+if platform == "darwin":
+    import os
 
-import os
+    os.environ.setdefault("KMP_DUPLICATE_LIB_OK", "TRUE")
 
-os.environ.setdefault("KMP_DUPLICATE_LIB_OK", "TRUE")
+    # Disable multithreading on MacOS arm64 since it causes FAISS to crash.
+    os.environ["OMP_NUM_THREADS"] = "1"
+    os.environ["OPENBLAS_NUM_THREADS"] = "1"
+    os.environ["MKL_NUM_THREADS"] = "1"
